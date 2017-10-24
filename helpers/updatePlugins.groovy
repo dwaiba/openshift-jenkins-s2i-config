f449fdb550d8151ee9e725217b647a1e069879bd
Deven Phillips <deven.phillips@redhat.com>
2017-10-24 12:45:16 -0400

    Perform version checks for plugins
    Ensures that plugins which require newer
    versions of Jenkins will not be updated

--- helpers/updatePlugins.groovy
+++ helpers/updatePlugins.groovy
@@ -4,6 +4,12 @@ import groovy.json.JsonSlurper
 
 import java.util.stream.Collectors
 
+def jenkinsVersion = [
+    major: 2,
+    minor: 46,
+    micro: 3
+]
+
 final def PLUGINS_API_BASE_URL = 'https://plugins.jenkins.io/api/plugin/'
 
 def newPlugins = []
@@ -26,24 +32,39 @@ if (!(inputList.exists() && inputList.canRead())) {
 if (inputList) {    // User DID pass an input file as an argument
     inputList.readLines()
              .parallelStream()
-             .map({line -> line.tokenize( ':' )[0]})
-             .map({name -> new URL("${PLUGINS_API_BASE_URL}${name}")})
-             .forEach { url ->  // Iterate over input file lines
-        def conn = url.openConnection()
+             .map({line -> line.tokenize( ':' )})
+             .map({name -> [
+                    url: "${PLUGINS_API_BASE_URL}${name[0]}".toString(),
+                    name: name[0],
+                    version: name[1]
+                ]})
+             .forEach { input ->  // Iterate over input file lines
+        def conn = new URL(input.url).openConnection()
         conn.addRequestProperty("Accept", "application/json")
         conn.with {
             requestMethod = 'GET'
         }
         try {
             if (conn.getResponseCode() == 200) {
-                def jsonResponse = parser.parseText((String)conn.getInputStream().text)
+                def jsonResponse = parser.parseText((String) conn.getInputStream().text)
 
-                newPlugins.add("${jsonResponse.name}:${jsonResponse.version}")
+                def minVersion = [
+                    major: jsonResponse.requiredCore.tokenize('.')[0] as Integer,
+                    minor: jsonResponse.requiredCore.tokenize('.')[1] as Integer,
+                    micro: (jsonResponse.requiredCore.tokenize('.')[2] ?: 0) as Integer
+                ]
+                if (minVersion.major >= jenkinsVersion.major && minVersion.minor >= jenkinsVersion.minor && minVersion.micro >= jenkinsVersion.micro) {
+                    newPlugins.add("${jsonResponse.name}:${jsonResponse.version}")
+                    println "Updated ${jsonResponse.name} to ${jsonResponse.version}"
+                } else {
+                    newPlugins.add("${input.name}:${input.version}")
+                    println "Keeping ${input.name} at ${input.version}"
+                }
             } else {
-                println "Error accessing Jenkins Plugins API: ${url.toString()}"
+                println "Error accessing Jenkins Plugins API: ${input.url.toString()}"
             }
         } catch (Exception e) {
-            println "Error retrieving data from Plugins API: ${url.toString()}"
+            println "Error retrieving data from Plugins API: ${input.url.toString()}"
             e.printStackTrace()
         }
     }
@@ -59,7 +80,11 @@ if (inputList) {    // User DID pass an input file as an argument
             .sort({ a,b -> a.toLowerCase().compareTo(b.toLowerCase())})
             .stream()
             .collect(Collectors.joining('\n'))
-    output.write(pluginsText)
+    if (output) {
+        output.write(pluginsText)
+    } else {
+        println pluginsText
+    }
 } else {
     println "You MUST provide an absolute or relative path to an input file for this script to work."
     println "EX: ./updatePlugins.groovy /path/to/plugins.txt"
